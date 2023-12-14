41b5a3578c9c50020822feef0e7e0afc3f4b40a9
Faelixx <sasha.taylor7@gmail.com>
2023-12-14 08:39:43 -0700

    Feat: Scan databse and dynamically add all tweets found to page, hard coded

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,57 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) { 
+    let $renderedTweet = createTweetElement(tweet);
+    $('#all-tweets').append($renderedTweet);
+  };
 };
 
-const createTweetElement = function () {
-  const result = $(`
+const createTweetElement = function (tweet) {
+  let $tweet = $(`
   <article class ="tweet">
           <div class="tweet-box">
             <header>
               <div class="tweet-profile">
-                <img id="tweet-profile-pic"  src="${tweetData["user"]["avatars"]}">
-                <p id="tweet-display-name">${tweetData["user"]["name"]}</p>
+                <img id="tweet-profile-pic"  src="${tweet["user"]["avatars"]}">
+                <p id="tweet-display-name">${tweet["user"]["name"]}</p>
                 <div class="tweet-username">
-                  <p>${tweetData["user"]["handle"]}</p>
+                  <p>${tweet["user"]["handle"]}</p>
                 </div>
               </div>
             </header>
-              <p id="tweet-body">${tweetData["content"]["text"]}</p>
+              <p id="tweet-body">${tweet["content"]["text"]}</p>
               <hr>
               <footer>
                 <div class="tweet-age-share">
-                  <p>${tweetData["created_at"]}</p>
+                  <p>${tweet["created_at"]}</p>
                   <div class="rep-share-like">
                     <p><i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i> </p>
                   </div>
@@ -42,11 +63,7 @@ const createTweetElement = function () {
           </div>
         </article>
   `)
-  return result;
+  return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData);
-
-console.log($tweet);
-
-$('#all-tweets').append($tweet);
+renderTweets(tweetData);
